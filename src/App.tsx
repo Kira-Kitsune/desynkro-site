2a54c1f42c1a74756c7381dbb29a6b911c90da74
Kira Kitsune <kira-pearce@outlook.com>
2024-03-20 10:46:19 +0100

    Updated routing and packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,94 +1,16 @@
-import { lazy, type Component } from 'solid-js';
-import Navbar from './components/Navbar';
-import { Route, Routes } from '@solidjs/router';
-import Home from './pages/Home';
-import _404 from './pages/_404';
-import Portfolio from './pages/Portfolio';
-const PortfolioSection = lazy(() => import('./pages/PortfolioSection'));
-const Album = lazy(() => import('./pages/Album'));
+import type { RouteSectionProps } from '@solidjs/router';
+import type { Component } from 'solid-js';
 import Footer from './components/Footer';
-import {
-    AnimalPortfolio,
-    CosplayPortfolio,
-    ModelPortfolio,
-    VehiclePortfolio,
-    StreetPortfolio,
-    OtherPortfolio,
-} from './utils/portfolios';
-import { PixelExpo2023 } from './utils/albums';
+import Navbar from './components/Navbar';
 
-const App: Component = () => {
+const App: Component<RouteSectionProps<unknown>> = (props) => {
     return (
         <>
             <Navbar />
             <div class="fixed z-[45] flex h-8 w-full items-center justify-center bg-yellow-400 text-center text-xl">
                 Website is currently in Beta!
             </div>
-            <Routes>
-                <Route path="/" component={Home} />
-                <Route path="/portfolio" component={Portfolio} />
-                <Route
-                    path="/portfolio/animals"
-                    element={
-                        <PortfolioSection
-                            folder={AnimalPortfolio}
-                            title="Pets/Animals"
-                        />
-                    }
-                />
-                <Route
-                    path="/portfolio/cosplays"
-                    element={
-                        <PortfolioSection
-                            folder={CosplayPortfolio}
-                            title="Cosplays"
-                        />
-                    }
-                />
-                <Route
-                    path="/portfolio/models"
-                    element={
-                        <PortfolioSection
-                            folder={ModelPortfolio}
-                            title="Models"
-                        />
-                    }
-                />
-                <Route
-                    path="/portfolio/vehicles"
-                    element={
-                        <PortfolioSection
-                            folder={VehiclePortfolio}
-                            title="Vehicles"
-                        />
-                    }
-                />
-                <Route
-                    path="/portfolio/street"
-                    element={
-                        <PortfolioSection
-                            folder={StreetPortfolio}
-                            title="Street Photography"
-                        />
-                    }
-                />
-                <Route
-                    path="/portfolio/other"
-                    element={
-                        <PortfolioSection
-                            folder={OtherPortfolio}
-                            title="Other Photos"
-                        />
-                    }
-                />
-                <Route
-                    path="/portfolio/album/pixel_expo_2023"
-                    element={
-                        <Album folder={PixelExpo2023} title="Pixel Expo 2023" />
-                    }
-                />
-                <Route path="*" component={_404} />
-            </Routes>
+            {props.children}
             <Footer />
         </>
     );
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,100 @@
 /* @refresh reload */
 import './index.css';
 import { render } from 'solid-js/web';
-import { Router } from '@solidjs/router';
+import { Router, Route } from '@solidjs/router';
 
 import App from './App';
-import type { Component } from 'solid-js';
+import { lazy, type Component } from 'solid-js';
+
+const PortfolioSection = lazy(() => import('./pages/PortfolioSection'));
+const Album = lazy(() => import('./pages/Album'));
+
+import { PixelExpo2023 } from './utils/albums';
+import {
+    AnimalPortfolio,
+    CosplayPortfolio,
+    ModelPortfolio,
+    OtherPortfolio,
+    StreetPortfolio,
+    VehiclePortfolio,
+} from './utils/portfolios';
+import Home from './pages/Home';
+import Portfolio from './pages/Portfolio';
+import _404 from './pages/_404';
 
 const WrappedApp: Component = () => {
     return (
-        <Router>
-            <App />
+        <Router root={App}>
+            <Route path="/" component={Home} />
+            <Route path="/portfolio">
+                <Route path="/" component={Portfolio} />
+                <Route
+                    path="/animals"
+                    component={() => (
+                        <PortfolioSection
+                            folder={AnimalPortfolio}
+                            title="Pets & Animals"
+                        />
+                    )}
+                />
+                <Route
+                    path="/cosplays"
+                    component={() => (
+                        <PortfolioSection
+                            folder={CosplayPortfolio}
+                            title="Cosplays"
+                        />
+                    )}
+                />
+                <Route
+                    path="/models"
+                    component={() => (
+                        <PortfolioSection
+                            folder={ModelPortfolio}
+                            title="Models"
+                        />
+                    )}
+                />
+                <Route
+                    path="/vehicles"
+                    component={() => (
+                        <PortfolioSection
+                            folder={VehiclePortfolio}
+                            title="Vehicles"
+                        />
+                    )}
+                />
+                <Route
+                    path="/street"
+                    component={() => (
+                        <PortfolioSection
+                            folder={StreetPortfolio}
+                            title="Street Photography"
+                        />
+                    )}
+                />
+                <Route
+                    path="/other"
+                    component={() => (
+                        <PortfolioSection
+                            folder={OtherPortfolio}
+                            title="Other Photos"
+                        />
+                    )}
+                />
+                <Route path="/album">
+                    <Route
+                        path="/pixel_expo_2023"
+                        component={() => (
+                            <Album
+                                folder={PixelExpo2023}
+                                title="Pixel Expo 2023"
+                            />
+                        )}
+                    />
+                </Route>
+            </Route>
+            <Route path="*" component={_404} />
         </Router>
     );
 };
